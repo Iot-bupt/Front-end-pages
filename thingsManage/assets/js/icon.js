f4d9f6c231a850488f3370c0738d50de09a592b6
ly547165949 <l19960205>
2018-04-18 15:51:27 +0800

    突出显示效果

--- thingsManage/assets/js/icon.js
+++ thingsManage/assets/js/icon.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $(".homeIconBackground,ul li").mouseover(function(e){
+    $(".homeIconBackground,.side-menu-icon").mouseover(function(e){
         $(this).siblings().stop().fadeTo(300, 0.5);//动画速度用数字表示时不需加引号
     });
-    $(".homeIconBackground,ul li").mouseout(function () {
+    $(".homeIconBackground,.side-menu-icon").mouseout(function () {
         $(this).siblings().stop().fadeTo(300, 1);
     });
 });
